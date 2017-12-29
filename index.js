72f1bd5ad0a8829f3da5ba132e1cb69023114df6
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-29 00:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+  return delete newObj.key;
+  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
